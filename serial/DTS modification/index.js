a254c422003aa3ef07aa462f4674bac66d842022
Aditya Ayachit <indiaaditya@gmail.com>
2020-11-23 13:50:05 +0530

    Added Serial Port

---
+++ serial/DTS modification/index.js
@@ -0,0 +1,11 @@
+const serialPort = require('serialport');
+
+var ports;
+async function getListOfSerialPorts() {
+    let list = await serialPort.list();
+    console.log(list);
+}
+
+
+getListOfSerialPorts();
+
